e63489c07fa6fea9f36e1bb4fe209d862dd9925f
Angelo Teixeira <imnotteixeira@gmail.com>
2020-10-24 15:46:55 +0100

    Added PausableTimer helper class

--- src/hooks/useTimeout.js
+++ src/hooks/useTimeout.js
@@ -1,1 +1,23 @@
-// TODO
+class PausableTimer {
+
+    constructor(callback, delay) {
+        let timerId, start, remaining = delay;
+
+        this.pause = function() {
+            clearTimeout(timerId);
+            remaining -= Date.now() - start;
+        };
+
+        this.resume = function() {
+            start = Date.now();
+            clearTimeout(timerId);
+            timerId = setTimeout(callback, remaining);
+        };
+
+        this.cancel = function() {
+            clearTimeout(timerId);
+        };
+
+        this.resume();
+    }
+}
