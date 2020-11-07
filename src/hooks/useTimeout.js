420234283fac47f6160a0486c3f655b925c11888
Angelo Teixeira <imnotteixeira@gmail.com>
2020-11-07 15:28:17 +0000

    Added useTimeout hook

--- src/hooks/useTimeout.js
+++ src/hooks/useTimeout.js
@@ -1,3 +1,5 @@
+const { useRef, useCallback, useEffect } = require("react");
+
 class PausableTimer {
 
     constructor(callback, delay) {
@@ -21,3 +23,41 @@ class PausableTimer {
         this.resume();
     }
 }
+
+const useTimeout = (callback, timeout) => {
+    const timer = useRef();
+
+    const handleCancel = useCallback(
+        () => {
+            if (timer.current) {
+                timer.current.cancel();
+                timer.current = undefined;
+            }
+        },
+        [timer],
+    );
+
+    // Ensures that the passed callback is stable and does not change between re-renders,
+    // triggering multiple executions of the timer
+    const safeCallback = useCallback(callback, [timer]);
+
+    useEffect(() => {
+        timer.current = new PausableTimer(safeCallback, timeout);
+        return handleCancel;
+    }, [safeCallback, handleCancel, timeout]);
+
+    const handlePause = () => {
+        if (timer.current) timer.current.pause();
+    };
+    const handleResume = () => {
+        if (timer.current) timer.current.resume();
+    };
+
+    return {
+        cancel: handleCancel,
+        pause: handlePause,
+        resume: handleResume,
+    };
+};
+
+export { useTimeout };
--- src/pages/DemoApp.js
+++ src/pages/DemoApp.js
@@ -1,10 +1,32 @@
-import React from "react";
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
 import UndoableWidget from "../components/ShoppingList/UndoableWidget";
 import CenteredComponent from "../components/utils/CenteredComponent";
+import { useTimeout } from "../hooks/useTimeout";
+
+const DemoTimer = () => {
+    const [completed, setCompleted] = useState(false);
+    const { resume, pause, cancel } = useTimeout(
+        () => {
+            setCompleted(true);
+        },
+        3000
+    );
+
+    return (
+        <>
+            {completed ? "RIIIING" : "Tik...Tok...Tik...Tok..."}
+            <Button onClick={pause}>Pause</Button>
+            <Button onClick={resume}>Resume</Button>
+            <Button onClick={cancel}>Cancel</Button>
+        </>
+    );
+};
 
 const DemoApp = () => (
     <>
         <CenteredComponent>
+            <DemoTimer />
             <UndoableWidget />
         </CenteredComponent>
     </>
